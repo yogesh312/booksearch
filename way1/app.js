756d77085cb52e64a42193426b782b10b1da06cd
yogesh <etpr.yogesh@gmail.com>
2021-10-13 02:16:00 +0530

    app and index file with full working app

---
+++ way1/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cookieParser = require('cookie-parser')
+
+
+const app = express();
+
+app.use(cookieParser())
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Setup Cross Origin
+app.use(require("cors")());
+
+
+
+//Bring in the routes
+app.use("/api", require("./routes/book"));
+
+
+//Setup Error Handlers
+const errorHandlers = require("./handlers/errorHandlers");
+app.use(errorHandlers.notFound);
+app.use(errorHandlers.mongoseErrors);
+if (process.env.ENV === "DEVELOPMENT") {
+  app.use(errorHandlers.developmentErrors);
+} else {
+  app.use(errorHandlers.productionErrors);
+}
+
+module.exports = app;
---
+++ way1/index.js
@@ -0,0 +1,29 @@
+const mongoose= require('mongoose')
+require("dotenv").config();
+
+
+mongoose.connect(process.env.DATABASE, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+  
+mongoose.connection.on("error", (err) => {
+    console.log("Mongoose Connection ERROR: " + err.message);
+});
+  
+mongoose.connection.once("open", () => {
+    console.log("MongoDB Connected!");
+});
+//Bring in the models
+require("./models/Book");
+
+
+const app = require("./app");
+
+const server = app.listen(8000, () => {
+  console.log("Server listening on port 8000");
+});
+
+
+
+
